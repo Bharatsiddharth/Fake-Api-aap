04cd2124fae88985d889c0f3f2d44662a11782d8
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-07-22 14:45:13 +0530

    dynamic data added to details

--- src/component/Details.jsx
+++ src/component/Details.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useParams } from 'react-router-dom'
+// import { ProductContext } from '../utils/context'
+import axios from '../utils/axios'
+import {  useEffect, useState } from 'react'
+import Loading from './Loading'
 
 const Details = () => {
-  return (
+    const [product, setproduct] = useState(null)
+    const { id } = useParams()
+    console.log(id)
+
+    const getSingleProduct = async () => {
+        try {
+            const { data } = await axios.get(`/products/${id}`);
+            setproduct(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getSingleProduct();
+    })
+
+  return product ? (
     <>
         <div className='w-[70%] h-full m-auto justify-between p-[10%] flex '>
-            <img className='h-[80%] w-[40%]  object-contain' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+            <img className='h-[80%] w-[40%]  object-contain' src={`${product.image}`} alt="" />
             <div className='content  w-[50%]'>
-                <h1 className='text-4xl'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-                <h3 className='text-zinc-400 my-5'>men's clothing</h3>
-                <h2 className='text-red-300 mb-3'>109.95$</h2>
+                <h1 className='text-4xl'>{`${product.title}`}</h1>
+                <h3 className='text-zinc-400 my-5'>{`${product.category}`}</h3>
+                <h2 className='text-red-300 mb-3'>$ {`${product.price}`}</h2>
                 
-                <p className='mb-[5%]'>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                <p className='mb-[5%]'>{`${product.description}`}</p>
                 <Link className='mr-5 py-2 px-5 border rounded mb-3 border-blue-200 text-blue-400'>Edit</Link>
                 <Link className='py-2 px-5 border rounded mb-3 border-red-200 text-red-400'>Delete</Link>
             </div>
         </div>
     </>
-  )
+  ): <Loading/>
 }
 
 export default Details
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,27 +2,40 @@
 import { Link } from "react-router-dom"
 import Nav from "./Nav"
 
+import { ProductContext } from "../utils/context"
+import { useContext } from "react"
+import Loading from "./Loading"
+
 
 const Home = () => {
-    return (
+    const [products] = useContext(ProductContext)
+    console.log(products)
+    return products ? (
         <>
-        <Nav />
+            <Nav />
             <div className=' w-[85%]  p-10 pt-[5%] flex flex-wrap gap-5 overflow-x-hidden overflow-y-auto'>
 
-                <Link to="/details/1" className=' card p-3 border shadow w-[18%] h-[30vh] flex-col flex justify-center items-center '>
+                {products.map((p, i) => (
 
-                    <div className='hover:scale-105 transition-all duration-500 ease-in-out mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center'
-                        style={{ backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" }}
-                    ></div>
-                    <h1>Lorem ipsum dolor sit amet.</h1>
 
-                </Link>
+                    <Link key={p.id} to={`/details/${p.id}`} className=' card p-3 border shadow w-[18%] h-[30vh] flex-col flex justify-center items-center '>
+
+                        <div className='hover:scale-105 transition-all duration-500 ease-in-out mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center'
+                            style={{ backgroundImage: `url(${p.image})` }}
+                        ></div>
+                        <h1>{p.title}</h1>
+
+                    </Link>
+
+                ))}
+
+
 
 
 
             </div>
         </>
-    )
+    ) : <Loading />
 }
 
 export default Home
